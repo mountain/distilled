881d7ba2eb5d73122b593f09ac5c094c0ce2d248
Mingli Yuan <mingli.yuan@gmail.com>
2011-01-26 12:26:45 +0800

    first commit of editor

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -3,23 +3,78 @@ function editor() {
     var cur = null;
 
     function highlight() {
-        var self = $(this);
-        if(cur === self) {
-            if (cur.css('border')) {
-                cur.css('border', null);
-                cur = null;
-            } else {
-                cur.css('border', '1px solid black');
-            }
-        } else {
-            if (cur && cur.css('border')) {
-                cur.css('border', null);
-            }
-            cur = self;
-            cur.css('border', '1px solid black');
+        if (cur && cur.hasClass('highlighted')) {
+            cur.removeClass('highlighted');
         }
+        cur = $(this);
+        cur.addClass('highlighted');
     }
 
     $('.pages > p').click(highlight);
     $('.pages > div').click(highlight);
+    $('.pages > table').click(highlight);
+    $('.pages > ul').click(highlight);
+    $('.pages > ol').click(highlight);
+
+    $('.tool').hover(function () {
+        $(this).addClass("toolhover");
+    },
+    function () {
+        $(this).removeClass("toolhover");
+    });
+    $('.tool_cr').click(function () {
+        if (cur) {
+            cur.append($('<br/>'));
+        }
+    });
+    $('.tool_del').click(function () {
+        if (cur) {
+            cur.remove();
+            cur = null;
+        }
+    });
+    $('.tool_left').click(function () {
+        if (cur) {
+            if (cur.hasClass('center')) {
+                cur.removeClass('center');
+            }
+            if (cur.hasClass('right')) {
+                cur.removeClass('right');
+            }
+            cur.addClass('left');
+        }
+    });
+    $('.tool_center').click(function () {
+        if (cur) {
+            if (cur.hasClass('left')) {
+                cur.removeClass('left');
+            }
+            if (cur.hasClass('right')) {
+                cur.removeClass('right');
+            }
+            cur.addClass('center');
+        }
+    });
+    $('.tool_right').click(function () {
+        if (cur) {
+            if (cur.hasClass('center')) {
+                cur.removeClass('center');
+            }
+            if (cur.hasClass('left')) {
+                cur.removeClass('left');
+            }
+            cur.addClass('right');
+        }
+    });
+    $('.tool_prev').click(function () {
+        if (cur) {
+            cur.prev().before(cur.detach());
+        }
+    });
+    $('.tool_next').click(function () {
+        if (cur) {
+            cur.next().after(cur.detach());
+        }
+    });
+
 }
