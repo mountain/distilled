bd9a4cfd5cd41f8130dc3742a4bc6302d0ee47e5
Mingli Yuan <mingli.yuan@gmail.com>
2011-02-12 18:31:55 +0800

    finish issues

---
+++ app/controllers/issues/index.js
@@ -0,0 +1,25 @@
+module.exports = function (env) {
+    var calendar = env.templates.issues.calendar;
+    var ga = env.analytics.id;
+    return function (req, res, next) {
+
+        var now = new Date(),
+            year = now.getUTCFullYear(),
+            month = now.getUTCMonth() + 1;
+
+        var cur = new Date(year, month - 1).getTime(),
+            prevmonth = new Date(cur - 15 * 24 * 3600 * 1000),
+            nextmonth = new Date(cur + 45 * 24 * 3600 * 1000);
+
+        prevmonth = '/issues/calendar/' + prevmonth.getUTCFullYear() + '/' +
+                    (prevmonth.getUTCMonth() + 1);
+
+        nextmonth = '/issues/calendar/' + nextmonth.getUTCFullYear() + '/' +
+                    (nextmonth.getUTCMonth() + 1);
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(calendar({year: year, month: month, prevmonth: prevmonth, nextmonth: nextmonth, ga: ga}));
+    };
+};
--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,7 @@ exports.settings = function (r) {
     r.get("/").to("main");
     r.get("/styles/magazine.css").to("style");
 
+    r.namespace("issues").get("/").to("issues/index");
     r.namespace("issues").get("/calendar/:year/:month").to("issues/calendar");
     r.namespace("issues").get("/:year/:month/:day").to("issues/issue");
 
