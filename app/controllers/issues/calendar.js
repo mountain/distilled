1965915eb4116ce1fea1405bdc40558909460374
Mingli Yuan <mingli.yuan@gmail.com>
2011-02-12 18:25:27 +0800

    calendar redesign

--- app/controllers/issues/calendar.js
+++ app/controllers/issues/calendar.js
@@ -2,11 +2,22 @@ module.exports = function (env) {
     var calendar = env.templates.issues.calendar;
     var ga = env.analytics.id;
     return function (req, res, next) {
-        var year = req.params.year;
-        var month = req.params.month;
+        var year = req.params.year,
+            month = req.params.month;
+
+        var cur = new Date(year, month - 1).getTime(),
+            prevmonth = new Date(cur - 15 * 24 * 3600 * 1000),
+            nextmonth = new Date(cur + 45 * 24 * 3600 * 1000);
+
+        prevmonth = '/issues/calendar/' + prevmonth.getUTCFullYear() + '/' +
+                    (prevmonth.getUTCMonth() + 1);
+
+        nextmonth = '/issues/calendar/' + nextmonth.getUTCFullYear() + '/' +
+                    (nextmonth.getUTCMonth() + 1);
+
         res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-        res.end(calendar({year: year, month: month, ga: ga}));
+        res.end(calendar({year: year, month: month, prevmonth: prevmonth, nextmonth: nextmonth, ga: ga}));
     };
 };
--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -2,25 +2,25 @@ function calendar(date) {
     date = date || new Date();
 
     function thumb(src, width) {
-        return 'http://commons.wikimedia.org/w/thumb.php?f=' + src.replace(' ', '_') + '&w=' + Math.round(width);
+        return 'http://commons.wikimedia.org/w/thumb.php?f=' + src.replace(' ', '_') + '&w=' + Math.round(width - 2);
     }
 
-    var width = $('#doublepage').width() / 7 * 0.4;
-    var height = $('#doublepage').height() / 7 * 0.8;
+    var width = Math.round($('#calendar').width() / 7 * 0.4);
+    var height = Math.round(width * 1.236);
 
     var html = _.template(
         '<div id="cover-<%= title %>" title="<%= title %>" ' +
-          'style="background:<%= bg %>;width:40%;height:80%;" ' +
+          'style="background:<%= bg %>;width:<%= width %>px;height:<%= height %>px;" ' +
           'class="cover-thumb cover-thumb-<%= cover %> ' +
           '<% if (cover) { %>clickable<% } %>"' +
         '>' +
         '<div class="cover-thumb-u">维基日刊</div>' +
         '<div class="cover-thumb-v">维<br/>基<br/>日<br/>刊</div>' +
         '<div class="cover-thumb-w">' +
-        '<img src="<%= thumb(photo, ' + width + ') %>">' +
+        '<table><tr><td><div><img src="<%= thumb(photo, ' + width + ') %>"></div></td></tr></table>' +
         '</div>' +
         '</div>' +
-        '<div class="date"><%= month %>月<%= day %>日</div>'
+        '<% if (cover) { %><div class="date"><%= month %>月<%= day %>日</div><% } %>'
     );
 
     var yr = date.getUTCFullYear(),
@@ -37,7 +37,9 @@ function calendar(date) {
                 bg: cell.attr('bg'),
                 thumb: thumb,
                 month: cell.attr('title').split('/')[1],
-                day: cell.attr('title').split('/')[2]
+                day: cell.attr('title').split('/')[2],
+                width: width,
+                height: height
             };
 
             cell.html(html(ctx));
