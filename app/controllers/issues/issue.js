6651574146deda927c028b671dd1c9d63bc0ce77
Kai Chen <kaichenxyz@gmail.com>
2011-02-17 23:44:59 +0800

    tweak issue show process

--- app/controllers/issues/issue.js
+++ app/controllers/issues/issue.js
@@ -1,27 +1,37 @@
 module.exports = function (env) {
     var issue = env.templates.issues.issue;
     var ga = env.analytics.id;
+
     return function (req, res, next) {
-        var year = req.params.year;
-        var month = req.params.month;
-        var day = req.params.day;
-		
-		if (isNaN(day) || parseInt(day) < 0 || parseInt(day) > 31){
-            res.writeHead(404, {
-                'Content-Type': 'text/html'
-            });
-            res.end('');
-            return false;
-        }
-		
+        // prepare params
+        var year  = req.params.year
+          , month = req.params.month
+          , day   = req.params.day;
+
+        // process
         try {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
+            var path = require("path")
+              , dataPath = path.join(
+                  env.path, "public/issues", year, month, day
+                ) + ".json";
+
+            path.exists(dataPath, function (exists) {
+                if (exists) {
+                    var html = issue({year: year, month: month, day: day, ga: ga});
+                    respond(res, 200, html, 'text/html');
+                } else {
+                    respond(res, 404, 'Page Not Found', 'text/plain');
+                }
             });
-            res.end(issue({year: year, month: month, day: day, ga: ga}));
         } catch (e) {
+            //TODO log to error.log
             require('sys').puts(e);
             require('sys').puts(e.stack);
         }
     };
 };
+
+function respond (res, statusCode, body, contentType) {
+    res.writeHead(statusCode, { 'Content-Type': contentType });
+    res.end(body);
+}
